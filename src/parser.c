96bf0bccd83a3f26fda0673a66cd2c70c6afe082
Sander Mertens <sander.mertens8@gmail.com>
2017-02-05 11:57:39 -0800

    Pass subexpression data from x/parser to x/rule

--- src/parser.c
+++ src/parser.c
@@ -16,51 +16,56 @@ corto_int32 _x_parser_matchRoute(
     x_parser this,
     corto_route route,
     corto_stringseq pattern,
-    corto_any param)
+    corto_any param,
+    corto_any *routerData)
 {
 /* $begin(corto/x/parser/matchRoute) */
     x_rule rule = x_rule(route);
     regex_t *regex = (regex_t*)rule->compiledRegex;
-    regmatch_t *match = alloca(sizeof(regmatch_t) * (regex->re_nsub + 1));
+    int ret = -1;
 
-    int ret = regexec(regex, pattern.buffer[0], (size_t)regex->re_nsub + 1, match, 0);
+    if (regex) {
+        regmatch_t *match = alloca(sizeof(regmatch_t) * (regex->re_nsub + 1));
+        ret = regexec(regex, pattern.buffer[0], (size_t)regex->re_nsub + 1, match, 0);
 
-    if (!ret) {
-        corto_object result = corto_declare(rule->pattern);
+        if (!ret) {
+            corto_object result = corto_declare(rule->pattern);
 
-        int i;
-        for (i = 1; i <= regex->re_nsub; i++) {
-            x_pattern_parameter *p = corto_llGet(rule->pattern->params, i - 1);
-            corto_id substr;
-            char *substrPtr = substr;
-            memcpy(substr, &pattern.buffer[0][match[i].rm_so], match[i].rm_eo - match[i].rm_so);
-            substr[match[i].rm_eo - match[i].rm_so] = '\0';
+            int i;
+            for (i = 1; i <= regex->re_nsub; i++) {
+                x_pattern_parameter *p = corto_llGet(rule->pattern->params, i - 1);
+                corto_id substr;
+                char *substrPtr = substr;
+                memcpy(substr, &pattern.buffer[0][match[i].rm_so], match[i].rm_eo - match[i].rm_so);
+                substr[match[i].rm_eo - match[i].rm_so] = '\0';
 
-            /* Assign member of result object */
-            corto_value o, m, v, r;
-            o = corto_value_object(result, NULL);
-            corto_int16 ret = corto_value_getMember(&o, p->name, &m);
-            if (ret) {
-                corto_seterr("invalid parameter '%s': %s", p->name, corto_lasterr());
+                /* Assign member of result object */
+                corto_value o, m, v, r;
+                o = corto_value_object(result, NULL);
+                corto_int16 ret = corto_value_getMember(&o, p->name, &m);
+                if (ret) {
+                    corto_seterr("invalid parameter '%s': %s", p->name, corto_lasterr());
+                    corto_delete(result);
+                    goto error;
+                }
+
+                r = corto_value_init();
+                v = corto_value_value(corto_string_o, &substrPtr);
+                corto_value_binaryOperator(CORTO_ASSIGN, &m, &v, &r);
+                corto_value_free(&r);
+            }
+
+            if (corto_define(result)) {
                 corto_delete(result);
                 goto error;
             }
 
-            r = corto_value_init();
-            v = corto_value_value(corto_string_o, &substrPtr);
-            corto_value_binaryOperator(CORTO_ASSIGN, &m, &v, &r);
-            corto_value_free(&r);
+            routerData->type = corto_type(rule->pattern);
+            routerData->value = result;
+            
+        } else if (ret != REG_NOMATCH) {
+            printf("x: error matching regex\n");
         }
-
-        if (corto_define(result)) {
-            corto_delete(result);
-            goto error;
-        }
-
-        printf("%15s: %s\n", corto_idof(route), corto_contentof(NULL, "text/corto-color", result));
-
-    } else if (ret != REG_NOMATCH) {
-        printf("x: error matching regex\n");
     }
 
     return !ret ? 0 : -1;
--- src/rule.c
+++ src/rule.c
@@ -16,27 +16,38 @@ corto_int16 _x_rule_construct(
     x_rule this)
 {
 /* $begin(corto/x/rule/construct) */
+    if (corto_route(this)->pattern) {
+        this->pattern = corto_declareChild(corto_parentof(corto_parentof(this)), corto_idof(this), x_pattern_o);
+        corto_setstr(&this->pattern->expr, corto_route(this)->pattern);
+        corto_setref(&this->pattern->scope, corto_parentof(corto_parentof(this)));
+        if (corto_define(this->pattern)) {
+            goto error;
+        }
 
-    this->pattern = corto_declareChild(corto_parentof(corto_parentof(this)), corto_idof(this), x_pattern_o);
-    corto_setstr(&this->pattern->expr, corto_route(this)->pattern);
-    corto_setref(&this->pattern->scope, corto_parentof(corto_parentof(this)));
-    if (corto_define(this->pattern)) {
-        goto error;
+        corto_asprintf(&this->regex, "^%s$", this->pattern->regex);
+
+        /* Compile regular expression */
+        regex_t *regex = corto_alloc(sizeof(regex_t));
+        int ret = regcomp(regex, this->pattern->regex, REG_EXTENDED);
+        if (ret) {
+            corto_seterr("x: failed to compile regex '%s'\n", this->pattern->regex);
+            goto error;
+        }
+
+        this->compiledRegex = (corto_word)regex;
     }
 
-    corto_asprintf(&this->regex, "^%s$", this->pattern->regex);
-
-    /* Compile regular expression */
-    regex_t *regex = corto_alloc(sizeof(regex_t));
-    int ret = regcomp(regex, this->pattern->regex, REG_EXTENDED);
+    corto_int16 ret = corto_route_construct(this);
     if (ret) {
-        corto_seterr("x: failed to compile regex '%s'\n", this->pattern->regex);
         goto error;
     }
 
-    this->compiledRegex = (corto_word)regex;
+    /* Overwrite type of 'node' to match the type of the pattern */
+    if (this->pattern) {
+        corto_setref(&corto_function(this)->parameters.buffer[0].type, this->pattern);
+    }
 
-    return corto_route_construct(this);
+    return 0;
 error:
     return -1;
 /* $end */
